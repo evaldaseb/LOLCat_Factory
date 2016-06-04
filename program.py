c917ee70101722e1dccb472a523e9e76e9207c46
Evaldas <ewaldas@gmail.com>
2016-06-04 18:04:12 +0100

    Constructing cat_pictures folder and path

--- program.py
+++ program.py
@@ -1,7 +1,9 @@
+import os
+
 
 def main():
     print_header()
-    # get or create output folder
+    get_or_create_output_folder()
     # download cats
     # display cats
     print('hello from main')
@@ -11,6 +13,12 @@ def print_header():
     print('--------------------------')
     print('         CAT FACTORY')
     print('--------------------------')
+
+
+def get_or_create_output_folder():
+    folder = 'cat_pictures'
+    full_path = os.path.abspath(os.path.join('.', folder))
+    print(full_path)
 
 
 if __name__ == '__main__':
