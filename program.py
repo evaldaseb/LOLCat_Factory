f2f4dd42c1afc023586dfb173df5c3a72bd3d540
Evaldas <ewaldas@gmail.com>
2016-06-04 17:57:41 +0100

    Added print_header

--- program.py
+++ program.py
@@ -1,10 +1,16 @@
 
 def main():
-    # print the header
+    print_header()
     # get or create output folder
     # download cats
     # display cats
     print('hello from main')
+
+
+def print_header():
+    print('--------------------------')
+    print('         CAT FACTORY')
+    print('--------------------------')
 
 
 if __name__ == '__main__':
